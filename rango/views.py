cc23a21f2823d1d6dd43ea3ae6ef6574b03f1b8a
Rahil Parikh <rahilparikh11@gmail.com>
2015-10-24 13:20:43 +0530

    Initial commit

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -34,8 +35,61 @@ def category(request, category_name_slug):
 		context_dict['pages'] = pages
 
 		context_dict['category'] = category
+		context_dict['category_name_slug'] = category_name_slug
 
 	except Category.DoesNotExist:
 		pass
 
 	return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			cat = form.save(commit = True)
+			print cat, cat.slug
+			return index(request)
+
+		else:
+			print form.errors
+
+	else:
+		form = CategoryForm()
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+	try:
+		cat = Category.objects.get(slug = category_name_slug)
+
+	except Category.DoesNotExist:
+		cat = None
+
+	if request.method=='POST':
+		form = PageForm(request.POST)
+
+		if form.is_valid():
+			if cat:
+				page = form.save(commit = False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request, category_name_slug)
+
+		else:
+			print form.errors
+
+	else:
+		form = PageForm()
+
+	context_dict = {'form': form, 'category': cat, 'category_name_slug': category_name_slug}
+
+	return render(request, 'rango/add_page.html', context_dict)
+
+
+
+
+
